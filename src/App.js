603ec5ae99d8e920d8285f0db9637bfe68f60246
Jose Moreno Gomez <joseutp14@gmail.com>
2020-07-05 15:39:55 -0600

    add spinner in Appjs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
-import Form from './components/Form'
+import Form from './components/Form';
+import Message from './components/Message';
+import Results from './components/Results';
+import Spinner from './components/Spinner';
 
 
 function App() {
   //Define the State
-  const [quantity, saveQuantity]= useState(0);
-  const [term, saveTerm]= useState('');
-  const [total, saveTotal] = useState(0)
+  const [ quantity, saveQuantity ]= useState(0);
+  const [ term, saveTerm ]= useState('');
+  const [  total, saveTotal ] = useState(0);
+  const [ charging, saveCharging ] = useState(false);
 
+  let components;
+
+  if(charging){
+    components = <Spinner/>;
+  }
+  else if(total === 0){
+    components = <Message/>
+  }else{
+    components = <Results
+      total = {total}
+      term = {term}
+      quantity = {quantity}
+    />
+  }
 
   return ( 
     <Fragment>
@@ -21,10 +39,13 @@ function App() {
         saveQuantity = { saveQuantity }
         term = { term }
         saveTerm = { saveTerm }
-        total = { total }
         saveTotal = { saveTotal }
+        saveCharging = { saveCharging }
       />
-      <p>Total a pagar ${total}</p>
+      <div className="mensajes">
+      {components}
+      </div>
+      
       </div>
     
     </Fragment>
